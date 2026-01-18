cc2d54105f37352b4016a7912f738fe07575de22
Nitesh pawar <np370768@gmail.com>
2026-01-18 22:52:02 +0530

    remove root

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return ( 
         <div className='container p-5'>
             <div className='row text-center'>
-                <img src="public/media/images/image1.png"  alt="Hero Image" className='mb-5'/>
+                <img src="/media/images/image1.png"  alt="Hero Image" className='mb-5'/>
                 <h1 className='mt-5'>
                     Invest in everything
                 </h1>
