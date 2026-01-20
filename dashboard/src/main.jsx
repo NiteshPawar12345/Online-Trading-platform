5a0dc306d80dd77d8fc8575c3a545cff616d2c63
Nitesh pawar <np370768@gmail.com>
2026-01-20 13:14:26 +0530

    Add context

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
+import GeneralContext from "./components/GeneralContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
+      <GeneralContext>
+        <Routes>
         <Route path="/*" element={<Home />} />
 
       </Routes>
+      </GeneralContext>
     </BrowserRouter>
   </React.StrictMode>
 );
