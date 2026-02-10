5b57348a8313adf23da05ee5665223e33434a36e
Nitesh pawar <np370768@gmail.com>
2026-02-10 12:30:43 +0530

    URL

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -7,7 +7,8 @@ function Navbar() {
 
     const handleDashboardClick = (e) => {
         e.preventDefault();
-        window.location.href = "https://online-trading-platform-2bwifxe9q-niteshpawar12345s-projects.vercel.app/";
+        window.location.href = "https://online-trading-platform-teal.vercel.app/";
+        
     };
 
     return (
